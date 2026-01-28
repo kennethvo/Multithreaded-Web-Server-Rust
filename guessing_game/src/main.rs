1f5ddf630d507ae7212c6e725abf523b4e697770
Kenneth Vo <kennethvo2003@gmail.com>
2026-01-27 19:01:58 -0600

    added commenting to prev thing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,13 @@
-use std::io;
+use std::io; // importing libraries, this one being input output in standard library
 
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // var
 
-    io::stdin()
+    io::stdin() // user input. reads in val and error handles the result
         .read_line(&mut guess)
         .expect("Failed to read line");
     
